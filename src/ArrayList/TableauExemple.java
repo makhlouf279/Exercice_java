dadb298a5d29641fa7b7fe589e2fbb8c4ceebd04
makhlouf279 <70649716+makhlouf279@users.noreply.github.com>
2021-04-21 18:36:12 -0400

    Add new calss in  ArrayList package

---
+++ src/ArrayList/TableauExemple.java
@@ -0,0 +1,12 @@
+package ArrayList;
+public class TableauExemple {
+    public static void main(String[] args) {
+        int[]tab=new int[10];
+        tab[0]=1;
+        tab[1]=3;
+        //methode length() pour donner la taille de tableau
+        System.out.println("la taille de tableau est " +tab.length);
+        System.out.println(tab[0]);
+        System.out.println(tab[1]);
+    }
+}
